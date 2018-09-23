29bfa247346766cb328ef962f7fd7df6a95144a1
liquid0624 <rjbirnesser@gmail.com>
2018-09-23 01:10:21 -0400

    msm8998: import device parts package
    Credit: omnirom
    
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- common.mk
+++ common.mk
@@ -156,6 +156,10 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Device
+PRODUCT_PACKAGES += \
+    DeviceParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
