a2d240dc98860d2db54b0f5b45faa93a066f9c3c
syrkles <syrkles@gmail.com>
2018-11-24 04:01:44 +0000

    msm8998:fix my derp

--- common.mk
+++ common.mk
@@ -85,7 +85,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl:32 \
+    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     audio.a2dp.default \
